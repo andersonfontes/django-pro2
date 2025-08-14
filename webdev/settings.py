3c4293741cd393dd6e7029903beb20cae238fec5
anderson <anderson@anderson>
2025-08-14 16:38:44 -0300

    criado projeto contatos

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oticebihg1lgn7le*fel^n_v=m-@472$kn%#&lbwl&5ou%#8de
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["andersonfontes.pythonanywhere.com"]
+ALLOWED_HOSTS = ["andersonfontes.pythonanywhere.com","127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Aplicativo exemplo mycontacts
+    'mycontacts',
 ]
 
 MIDDLEWARE = [
